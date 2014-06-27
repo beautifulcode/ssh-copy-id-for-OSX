6e649dbe514c238fe2b9dcc82e96e38873f8952e
Joe Nyland <joenyland@me.com>
2014-06-27 12:21:41 +0100

    Improved install.sh.
    Allows the install script to be curled directly from GitHub for easy install:
    
    	curl http://URL/TO/install.sh | sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# Installs ssh-copy-id into /usr/local/bin
 
-cp ssh-copy-id.sh /usr/local/bin/ssh-copy-id;
-chmod +x /usr/local/bin/ssh-copy-id;
+if [[ $(id -u) != 0 ]]; then
+	if command -v sudo >/dev/null 2>&1; then
+		SUDO="sudo"
+	else
+		echo >&2 "Requires sudo but it's not installed. Aborting."
+		exit 1
+	fi
+fi
+
+if git ls-files >& /dev/null &&  [[ -f ssh-copy-id.sh ]]; then
+	$SUDO cp ssh-copy-id.sh /usr/local/bin/ssh-copy-id || { echo "Failed to install ssh-copy-id into /usr/local/bin."; exit 1; }
+else
+	$SUDO curl -L https://raw.github.com/beautifulcode/ssh-copy-id-for-OSX/master/ssh-copy-id.sh -o /usr/local/bin/ssh-copy-id || { echo "Failed to install ssh-copy-id into /usr/local/bin."; exit 1; }
+	$SUDO chmod +x /usr/local/bin/ssh-copy-id || { echo "Failed to install ssh-copy-id into /usr/local/bin."; exit 1; }
+fi
+echo "Installed ssh-copy-id into /usr/local/bin."; exit 0;
